f0b3bc96329034e7d55596084900eeeec5271fbf
krisna firdaus <krisnafirdaus@gmail.com>
2020-09-02 09:26:45 +0700

    ok

--- style.js
+++ style.js
@@ -1,2 +1,5 @@
-import { hewan, buah } from "data.js";
-import { karyawan } from "karyawan.js";
+import { hewan, buah } from "./data.js";
+import { karyawan } from "./karyawan.js";
+
+console.log(karyawan);
+console.log(hewan);
